32571b538b5dee943bda3eebd4c7e2c0479172fa
ryu609 <m.joynauth@gmail.com>
2018-01-31 13:16:36 +0400

    WCF xml rest service

---
+++ WcfRestfulApi/AdventureWorkService.svc.cs
@@ -0,0 +1,16 @@
+﻿using DAL;
+
+namespace WcfRestfulApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AdventureWorkService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select AdventureWorkService.svc or AdventureWorkService.svc.cs at the Solution Explorer and start debugging.
+    public class AdventureWorkService : IAdventureWorkService
+    {
+        private AdWorksEntities _db = new AdWorksEntities();
+   
+        public string GetPerson(string Id)
+        {
+            return _db.People.Find(int.Parse(Id)).FirstName;
+        }
+    }
+}
---
+++ WcfRestfulApi/IAdventureWorkService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WcfRestfulApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAdventureWorkService" in both code and config file together.
+    [ServiceContract]
+    public interface IAdventureWorkService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "xml/{id}")]
+        string GetPerson(string Id);
+    }
+}
